b5cabce7b1f1fceacc8b63dfecf3ef7c586c6120
Gabriele Panico <gabriele.panico@gmail.com>
2023-11-07 14:25:41 +0100

    minor improvements

--- src/app/utils/util.ts
+++ src/app/utils/util.ts
@@ -5,3 +5,18 @@ export enum PLAN_NAME {
 }
 
 export const URL_understanding_default_roles = 'https://gethelp.tiledesk.com/articles/understanding-default-roles/' // 'https://docs.tiledesk.com/knowledge-base/understanding-default-roles/'
+
+export function getWidgetWebInstallationScript(projectID: string, widgetBaseUrl: string): string {
+    return `    window.tiledeskSettings = {
+        projecid:: ${projectID}
+    };
+    (function(d, s, id) {
+        var w=window; var d=document; var i=function(){i.c(arguments);};
+        i.q=[]; i.c=function(args){i.q.push(args);}; w.Tiledesk=i;     
+        var js, fjs=d.getElementsByTagName(s)[0];  
+        if (d.getElementById(id)) return;
+        js=d.createElement(s); 
+        js.id=id; js.async=true; js.src="${widgetBaseUrl}launch.js";
+        fjs.parentNode.insertBefore(js, fjs);
+     }(document,'script','tiledesk-jssdk'));`
+}
