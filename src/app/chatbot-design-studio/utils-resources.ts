333998425c0476de96b60b162b809ee6c9572c6c
Gabriele Panico <gabriele.panico@gmail.com>
2024-02-01 13:21:13 +0100

    minor improvement

--- src/app/chatbot-design-studio/utils-resources.ts
+++ src/app/chatbot-design-studio/utils-resources.ts
@@ -1,20 +1,21 @@
-export const LOGOS_ITEMS: { [key: string] : { label: string, icon: string }} ={
-    COMPANY_LOGO: {label: 'Tiledesk',  icon: 'assets/logos/tiledesk_logo.svg'},
-    COMPANY_LOGO_NO_TEXT: {label: 'Tiledesk', icon: 'assets/logos/tiledesk_logo_no_text.svg'},
-    BASE_LOGO: {label: 'Tiledesk',  icon: 'assets/logos/tiledesk_logo.svg'},
-    BASE_LOGO_NO_TEXT: {label: 'Tiledesk', icon: 'assets/logos/tiledesk_logo_no_text.svg'},
-    BASE_LOGO_WHITE: { label: 'Tiledesk', icon: '"assets/logos/tiledesk-logo_new_white.svg'},
-    BASE_LOGO_WHITE_NO_TEXT: { label: 'Tiledesk', icon: '"assets/logos/tiledesk-logo_new_white.svg'}
-}
-
-export const BRAND_BASE_INFO: { [key: string] : any} ={
+export const BRAND_BASE_INFO: { [key: string] : string} ={
     COMPANY_NAME: "Tiledesk",
     BRAND_NAME: "Tiledesk",
     COMPANY_SITE_NAME:"tiledesk.com",
-    COMANY_SITE_URL:"https://www.tiledesk.com",
+    COMPANY_SITE_URL:"https://www.tiledesk.com",
     CONTACT_US_EMAIL: "support@tiledesk.com",
     FAVICON: "https://tiledesk.com/wp-content/uploads/2022/07/tiledesk_v13-300x300.png",
     META_TITLE:"Design Studio"
+}
+
+export const LOGOS_ITEMS: { [key: string] : { label: string, icon: string }} ={
+    COMPANY_LOGO: {label: BRAND_BASE_INFO.COMPANY_NAME,  icon: 'assets/logos/tiledesk_logo.svg'},
+    COMPANY_LOGO_NO_TEXT: {label: BRAND_BASE_INFO.COMPANY_NAME, icon: 'assets/logos/tiledesk_logo_no_text.svg'},
+    BASE_LOGO: {label: BRAND_BASE_INFO.BRAND_NAME,  icon: 'assets/logos/tiledesk_logo.svg'},
+    BASE_LOGO_NO_TEXT: {label: BRAND_BASE_INFO.BRAND_NAME, icon: 'assets/logos/tiledesk_logo_no_text.svg'},
+    BASE_LOGO_WHITE: { label: BRAND_BASE_INFO.BRAND_NAME, icon: '"assets/logos/tiledesk-logo_new_white.svg'},
+    BASE_LOGO_WHITE_NO_TEXT: { label: BRAND_BASE_INFO.BRAND_NAME, icon: '"assets/logos/tiledesk-logo_new_white.svg'},
+    BASE_LOGO_GRAY: { label: BRAND_BASE_INFO.BRAND_NAME, icon: 'https://support-pre.tiledesk.com/dashboard/assets/img/logos/tiledesk-logo_new_gray.svg'}
 }
 
 export const MEDIA: { [key: string]: { src: string, text: string, description: string}}= {
