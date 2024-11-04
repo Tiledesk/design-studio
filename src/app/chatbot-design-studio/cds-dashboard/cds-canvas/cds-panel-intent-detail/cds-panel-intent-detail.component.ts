476f01f2dc023ed0d0c0ab7105d01364d10b6a0c
Dario De Pascalis <dariodepa75@gmail.com>
2024-11-04 11:05:24 +0100

    open panel intent detail

--- src/app/chatbot-design-studio/cds-dashboard/cds-canvas/cds-panel-intent-detail/cds-panel-intent-detail.component.ts
+++ src/app/chatbot-design-studio/cds-dashboard/cds-canvas/cds-panel-intent-detail/cds-panel-intent-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { LoggerService } from 'src/chat21-core/providers/abstract/logger.service';
+import { LoggerInstance } from 'src/chat21-core/providers/logger/loggerInstance';
 
 @Component({
   selector: 'cds-panel-intent-detail',
@@ -6,22 +8,30 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./cds-panel-intent-detail.component.scss']
 })
 export class CdsPanelIntentDetailComponent implements OnInit {
-  @Input() elementIntentSelected: any;
+  @Input() intentSelected: any;
   @Input() showSpinner: boolean;
   @Output() savePanelIntentDetail = new EventEmitter();
   @Output() closePanel = new EventEmitter();
   
-
   maximize: boolean = false;
   agents_available: boolean = false;
 
+  private logger: LoggerService = LoggerInstance.getInstance();
   constructor() { }
 
   ngOnInit(): void {
+    this.agents_available = this.intentSelected.agents_available?this.intentSelected.agents_available:false;
   }
 
 
   onAgentsAvailableChange(event: any){
+    this.logger.log('[CdsPanelIntentDetailComponent] onAgentsAvailableChange:: ', event);
+    this.intentSelected.agents_available = event;
+    this.onSaveIntent();
+  }
 
+  private onSaveIntent(){
+    this.logger.log('[CdsPanelIntentDetailComponent] onSaveIntent:: ', this.intentSelected);
+    this.savePanelIntentDetail.emit(this.intentSelected);
   }
 }
