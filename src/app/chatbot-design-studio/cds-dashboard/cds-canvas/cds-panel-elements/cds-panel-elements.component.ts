3643673c68c95058972c54bf31119f8b348c0acb
Dario De Pascalis <dariodepa75@gmail.com>
2025-07-22 11:05:37 +0200

    Cancel voice Menu Compatibility Patch if no subtype is present

--- src/app/chatbot-design-studio/cds-dashboard/cds-canvas/cds-panel-elements/cds-panel-elements.component.ts
+++ src/app/chatbot-design-studio/cds-dashboard/cds-canvas/cds-panel-elements/cds-panel-elements.component.ts
@@ -119,12 +119,9 @@ export class CdsPanelElementsComponent implements OnInit {
 
   createActionListByCategory(){
     ACTION_CATEGORY.forEach(category => {
-      const subtype = this.dashboardService.selectedChatbot.subtype?this.dashboardService.selectedChatbot.subtype:null;
-      //subtype = null; //TYPE_CHATBOT.CHATBOT;
+      const subtype = this.dashboardService.selectedChatbot.subtype?this.dashboardService.selectedChatbot.subtype:TYPE_CHATBOT.CHATBOT;
       this.logger.log('[CDS-PANEL-ELEMENTS] subtype:: ', ACTIONS_LIST, subtype);
-      if(subtype){
-        this.projectPlanUtils.checkIfActionIsInChatbotType(subtype as TYPE_CHATBOT);
-      }
+      this.projectPlanUtils.checkIfActionIsInChatbotType(subtype as TYPE_CHATBOT);
       let menuItemsList = Object.values(ACTIONS_LIST).filter(el => (el.category === TYPE_ACTION_CATEGORY[category.type] && el.status !== 'inactive')).map(element => {
         return {
           type: TYPE_OF_MENU.ACTION,
