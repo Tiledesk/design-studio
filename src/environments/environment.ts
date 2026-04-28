38e0995fbe094ee831347f2c2023f9eea5048f9a
Dario De Pascalis <dariodepa75@gmail.com>
2026-04-28 12:41:39 +0200

    change env autocomplete

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,21 @@ export const environment = {
   production: true,
   t2y12PruGU9wUtEGzBJfolMIgK: 'CHANGEIT',
   VERSION: require('../../package.json').version,
-  remoteConfig: false, 
-  remoteConfigUrl: './design-studio-config.json',
+  remoteConfig: true, 
+  remoteConfigUrl: '/real_data/cds-config-aws-stage.json',
+  // remoteConfigUrl: './design-studio-config.json',
   apiUrl: 'CHANGEIT',
   widgetBaseUrl: 'CHANGEIT',
   dashboardBaseUrl: 'CHANGEIT',
+  whatsappTemplatesBaseUrl: 'CHANGEIT',
   wsUrl: 'ws://localhost:3000/',
   uploadEngine: 'native',
   baseImageUrl: 'CHANGEIT',
+  fileUploadAccept: "*/*",
   logLevel: 'error',
+  aiModels: 'CHANGEIT',
   storage_prefix: "CHANGEIT",
+  pineconeReranking: "CHANGEIT",
   firebaseConfig: {
     apiKey: 'CHANGEIT',
     authDomain: 'CHANGEIT',
@@ -21,5 +26,10 @@ export const environment = {
     messagingSenderId: 'CHANGEIT',
     appId: 'CHANGEIT',
     vapidKey: 'CHANGEIT'
+  },
+  chat21Config: {
+    appId: 'tilechat',
+    MQTTendpoint: 'mqtt://localhost:15675/ws', // MQTT endpoint
+    APIendpoint: 'http://localhost:8004/api'
   }
 };
